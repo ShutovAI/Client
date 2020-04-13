55163d617bca999507c95c0368bf72a0e0ddcc87
shutovai <Pentalgin09@mail.ru>
2020-04-13 21:40:35 +0300

    Commit 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,93 +15,14 @@ public class Main {
             switch (console.readLine()) {
                 case "1":
                     registration();
-//                    while (true) {
-//                    System.out.print("Введите логин и пароль! (!логин:пароль!) ");
-//                    String reg = console.readLine();
-//                    while (!reg.contains(":") || reg.startsWith(" ") || reg.startsWith(":")) {
-//                        System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (!логин:пароль!)");
-//                        reg = console.readLine();
-//                    }
-//                    String trueReg = reg.replaceAll("\\s", "");
-//                    reg = trueReg;
-//
-//                    PrintWriter writer0 = new PrintWriter(socket.getOutputStream());
-//                    writer0.println(reg + "REGISTRATION");
-//                    writer0.flush();
                     break;
-//                    BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                    String serverMessage = clientReader.readLine();
-//                    while (!serverMessage.equals("Регистация прошла успешно!")) {
-//                        System.out.println("Данный пользователь уже зарегистрирован! Повторите ввод:");
-//                        serverMessage = clientReader.readLine();
-//                        String out = console.readLine();
-//                        PrintWriter writer1 = new PrintWriter(socket.getOutputStream());
-//                        writer1.println(out);
-//                        writer1.flush();
-//                    }
                 case "2":
                     autentification();
-//                    System.out.print("Введите логин и пароль! (логин:пароль) ");
-//                    String aut = console.readLine();
-//                    while (!aut.contains(":") || aut.startsWith(" ") || aut.startsWith(":")) {
-//                        System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (!логин:пароль!)");
-//                        aut = console.readLine();
-//                    }
-//                    String trueAut = aut.replaceAll("\\s", "");
-//                    aut = trueAut;
-//                    PrintWriter writer2 = new PrintWriter(socket.getOutputStream());
-//                    writer2.println(aut + "AUT");
-//                    writer2.flush();
-//                    BufferedReader clientReader2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                    String mess = clientReader2.readLine();
-//                    System.out.println("QQQ " + mess);
-//                    if(mess.contains("Неверный логин или пароль!")){
-//
-//                    }
-
-//                    BufferedReader clientReader2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                    String serverMessage2 = clientReader2.readLine();
-//                    System.out.println(serverMessage2);
                     break;
                 case "3":
                     System.exit(0);
+                    break;
             }
-
-//            Thread w = new Thread(() -> {
-//                try {
-//                    BufferedReader serverReader = new BufferedReader(
-//                            new InputStreamReader(socket.getInputStream()));
-//                    while (true) {
-//                        System.out.println(serverReader.readLine());
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//            w.start();
-//
-//            while (true) {
-//                String message = console.readLine();
-//                Thread t = new Thread(() -> {
-//                    try {
-//                        PrintWriter writer = new PrintWriter(socket.getOutputStream());
-//                        writer.println(message);
-//                        writer.flush();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-//                t.start();
-//                t.join();
-//                if (message.startsWith("exit")) {
-//                    System.exit(0);
-//                    break;
-//                }
-//            }
-//
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -109,11 +30,10 @@ public class Main {
 
     public static void autentification() {
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-//            Socket socket = new Socket(IP, PORT);
             System.out.print("Введите логин и пароль! (логин:пароль) ");
             String aut = console.readLine();
             while (!aut.contains(":") || aut.startsWith(" ") || aut.startsWith(":")) {
-                System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (!логин:пароль!)");
+                System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (логин:пароль)");
                 aut = console.readLine();
             }
             String boom2 = aut.replaceAll("\\s", "");
@@ -123,13 +43,10 @@ public class Main {
             writer2.flush();
             BufferedReader clientReader2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String checkAut = clientReader2.readLine();
-            System.out.println("И-хаа! " + checkAut);
+            System.out.println(checkAut);
             if (checkAut.contains("Неверный логин или пароль!")) {
                 autentification();
             }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
             Thread w = new Thread(() -> {
                 try {
                     BufferedReader serverReader = new BufferedReader(
@@ -142,7 +59,6 @@ public class Main {
                 }
             });
             w.start();
-
             while (true) {
                 String message = console.readLine();
                 Thread t = new Thread(() -> {
@@ -169,10 +85,10 @@ public class Main {
 
     public static void registration() {
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.print("Введите логин и пароль! (!Логин:Пароль!) ");
+            System.out.print("Введите логин и пароль! (Логин:Пароль) ");
             String reg = console.readLine();
             while (!reg.contains(":") || reg.startsWith(" ") || reg.startsWith(":")) {
-                System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (!логин:пароль!)");
+                System.out.println("Не верная запись ввода!\nПопробуйте еще раз! (логин:пароль)");
                 reg = console.readLine();
             }
             String trueReg = reg.replaceAll("\\s", "");
@@ -182,13 +98,10 @@ public class Main {
             writer0.flush();
             BufferedReader clientReader2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String checkReg = clientReader2.readLine();
-            System.out.println("И-хаа! " + checkReg);
+            System.out.println(checkReg);
             if (checkReg.contains("Данный пользователь уже зарегистрирован!")) {
                 registration();
             }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
             Thread w = new Thread(() -> {
                 try {
                     BufferedReader serverReader = new BufferedReader(
@@ -201,7 +114,6 @@ public class Main {
                 }
             });
             w.start();
-
             while (true) {
                 String message = console.readLine();
                 Thread t = new Thread(() -> {
@@ -223,9 +135,6 @@ public class Main {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-    }
-    private void chooseNum(){
-
     }
 }
 
